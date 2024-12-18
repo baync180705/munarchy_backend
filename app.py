ca863a8f912a4b5fa43b388773470441250d124f
Budhayan Chakraborty <budhayanc2005@gmail.com>
2024-12-19 00:11:32 +0530

    feat: adds webhook endpoint

--- app.py
+++ app.py
@@ -155,7 +155,7 @@ async def paymentSuccess():
         itemList = item.split('=')
         responseDict[f'{itemList[0]}'] = itemList[1]
     
-    popKeys = ['udf3','udf4','udf5','udf6','udf7','udf8','udf9']
+    popKeys = ['udf3','udf4','udf5','udf6','udf7','udf8','udf9','udf10']
     for key in popKeys:
         responseDict.pop(key)
     responseDict.update({
@@ -176,6 +176,31 @@ async def paymentSuccess():
 async def paymentFailture():
     return redirect(f"{os.getenv('BASE_URL')}/unsuccessful")
 
+@app.route('/api/easebuzz_webhook', methods=['POST'])
+async def paymentFailture():
+    data = await request.get_json
+    if data['status'] == 'success':
+        popKeys = ['udf3','udf4','udf5','udf6','udf7','udf8','udf9','udf10']
+        for key in popKeys:
+            data.pop(key)
+        data.update({
+            "accomodation": responseDict.pop('udf1'),
+            "MUNARCHY_ID": responseDict.pop('udf2'),
+            "timeStamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        })
+        data['email'] = responseDict['email'].replace('%40','@')
+        try:
+            await registration_records.update_one({"MUNARCHY_ID":data['MUNARCHY_ID']},{"$set":{"pay_status":True}})
+            await payment_records.insert_one(data)
+            asyncio.create_task(paymentEmail(responseDict['firstname'],responseDict['email'],responseDict['net_amount_debit'],responseDict['accomodation']))
+            return make_response(jsonify({
+                'Message':'Payment processed successfully'
+            }),200)
+        except Exception as e:
+            return make_response(jsonify({"Error":"Failed in updating data. Please try again"}),500)
+    return make_response(jsonify({
+        'ERROR':'Your payment has failed'
+    }),400)
 @app.route('/api/allotment', methods=['POST'])
 async def portfolioAllotments():
     #expects the following keys: MUNARCHY_ID, name, email_id, committee, portfolio
