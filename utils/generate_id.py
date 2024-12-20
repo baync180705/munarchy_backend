54fbe495ae24c27ff78ed43ee900187bb21ee8a9
Budhayan Chakraborty <budhayanc2005@gmail.com>
2024-12-20 11:45:21 +0530

    rfac: changes munarchy id logic | conf: implements persistent storage for storing serial number and updates gitignore

--- utils/generate_id.py
+++ utils/generate_id.py
@@ -1,9 +1,20 @@
 from uuid import uuid4
+import os
 
 def generateTxnId():
     return f'IRMUN{str(uuid4()).replace("-", "")}'
 
 def generateMunarchyId(gender, exp, isSchoolStudent):
+    file_path = f'{os.path.abspath(os.path.join(os.getcwd(),'current_index.txt'))}'
+    if not os.path.exists(file_path):
+        with open(file_path, 'w') as f:
+            index = '00001'
+            f.write('1')
+    else:
+        with open(file_path, 'r+') as f:
+            context = f.read()
+            index = str(int(context) + 1).rjust(5,'0')
+            f.write(index)
     mapGenderToCode = {
         'male': '7',
         'female': '8'
@@ -15,4 +26,4 @@ def generateMunarchyId(gender, exp, isSchoolStudent):
         '11th and 12th':'5',
         '10th and below':'4'
     }
-    return f"IR25{mapIsSchoolStatusToCode[isSchoolStudent]}{mapGenderToCode[gender]}{str(exp)[0]}{str(uuid4())[0:8].replace('-','')}"
+    return f"IR25{mapIsSchoolStatusToCode[isSchoolStudent]}{mapGenderToCode[gender]}{str(exp)[0]}{index}"
